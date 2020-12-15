c26d265aac7919ea77bc9991fc5c7bee5a2b73fb
Norlander, Brian <brian.norlander@rakuten.com>
2020-12-15 10:05:48 +0900

    First commit

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,71 @@
+import moment from 'moment';
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expeneses';
+
+test('should set default state', () => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expense if id not found', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-1'
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[1], expenses[2]]);
+});
+
+// should add an expense
+test('should add an expense', () => {
+    const newExpense = {
+        id: '4',
+        description: 'Book',
+        note: '',
+        amount: 1000,
+        createdAt: moment(0).subtract(2, 'days').valueOf()
+    };
+    const action = {
+        type: 'ADD_EXPENSE',
+        expense: newExpense
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([...expenses, newExpense]);
+});
+
+// should edit an expense
+test('should edit an expense', () => {
+    const amount = 122000;
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: expenses[1].id,
+        updates: {
+            amount
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state[1].amount).toBe(amount);
+});
+
+// should not edit an expense if expense not found
+test('should not edit an expense if expense not found', () => {
+    const amount = 122000;
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: '-1',
+        updates: {
+            amount
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state[1].amount).toBe(expenses[1].amount);
+});
